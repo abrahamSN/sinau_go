e2ae68a2ab231b6a8f6634c7d977f811a575b473
SNmayer <abrahamsn.mayer@gmail.com>
2020-03-16 20:06:53 +0700

    Baru

--- main.go
+++ main.go
@@ -36,6 +36,11 @@ func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, er
 	return consignment, nil
 }
 
+// GetAll consignment
+func (repo *Repository) GetAll() []*pb.Consignment {
+	return repo.consignments
+}
+
 // Service should implement all of the methods to satisfy the service
 // we defined in our protobuf definition. You can check the interface
 // in the generated code itself for the exact method signatures etc
@@ -58,6 +63,12 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 	// Return matching the `Response` message we created in our
 	// protobuf definition.
 	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+// GetConsignment
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
+	consignments := s.repo.GetAll()
+	return &pb.Response{Consignment: consignments}, nil
 }
 
 func main() {
